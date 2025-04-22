cd91d1f2adfcb36ff4ae37446b2cec9a34530f93
jonathas <jonathas.sabino@gmail.com>
2025-04-22 17:06:52 -0300

    JSjesussalva

---
+++ index.js
@@ -0,0 +1,30 @@
+//alert("hello world");
+ 
+
+ let nome ="Jonathas"; // Declara uma variável com seu nome
+ let idade ="36"; // Declara uma variável idade
+
+ let comidafavorita 
+
+ console.log(comidafavorita);
+ 
+ comidafavorita ="ovo"
+
+ console.log(comidafavorita)
+
+nome= prompt('digite seu nome')
+console.log(nome)
+
+//string
+
+nome = "Jonathas"
+idade = "36"
+
+//Number
+
+idade = 36;
+let altura = 1.80
+
+// Boolean (true ou false)
+
+const desenvolvedorJavascript = true; //camelcase
