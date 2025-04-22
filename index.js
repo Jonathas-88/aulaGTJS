133374ddc702b16a8db6c34d3154687bae2b785f
jonathas <jonathas.sabino@gmail.com>
2025-04-22 17:11:00 -0300

    JS

--- index.js
+++ index.js
@@ -19,12 +19,18 @@ console.log(nome)
 
 nome = "Jonathas"
 idade = "36"
+console.log(typeof(idade))
+
+
 
 //Number
 
 idade = 36;
 let altura = 1.80
+console.log(typeof(altura))
 
 // Boolean (true ou false)
 
 const desenvolvedorJavascript = true; //camelcase
+
+
