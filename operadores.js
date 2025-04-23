b10ec8654a480a4c1c7aeedc7c64790e04333af2
jonathas <jonathas.sabino@gmail.com>
2025-04-23 17:06:39 -0300

    JV

---
+++ operadores.js
@@ -0,0 +1,35 @@
+//soma
+//subtração
+//multiplicação
+//divisão
+
+// let numero1 = Number(prompt('digite um número')) 
+// let numero2 = Number(prompt('digite outro número')) 
+
+let soma = numero1 + numero2
+let subtracao = numero1 - numero2
+let multiplicacao = numero1 * numero2
+let divisao = numero1 / numero2
+
+console.log(`a soma é ${soma}, a subtracao é ${subtracao}, a multiplicacao é ${multiplicacao}, a divisao é ${divisao}`)
+
+let outraSoma = '20' + 2 //concatenação output-> '202'
+let outrSubtração = '20' - 2 // a conversão ela ocorre nos outros casos, não para a soma
+
+//operadores de atribuição (=, +=, -=, *=, /=)
+
+let dado = 'alguma informação'
+
+let x = 5
+console.log(x)
+x = x + 5 // resposta 10
+console.log(x)
+x += 5 // resposta 10
+console.log(x)
+
+//operadores de comparação (<,>, >=,<=,==,!=,===,!==) retorno borleano (true ou false)
+
+//operadores lógicos ( &&, ||, !)
+
+
+
---
+++ tiposdedados.js
@@ -0,0 +1,44 @@
+// Tipos de dados primitivos
+
+// Stings
+const escola = "digital college"
+const curso = ""
+
+// Boolean ou Boleano ( true ou false)
+
+let x = 2
+let y = 4
+
+console.log(x>y) // operação de comparação output-> false
+
+// Number
+idade = 36
+
+console.log(typeof(NaN)) //Not a Number
+
+// Undefined
+
+let nomeCompleto //camelCase
+
+console.log(nomeCompleto)
+
+// Null
+
+//Conversões
+
+let numeroAlunos = Number ('15')
+
+console.log(numeroAlunos)
+
+//Concatenação de string (usar acento grave)
+
+const nomeMae =  'Antonia'
+const sobrenomeMae = 'Jucilene'
+
+let nomeCompletoMae = 'o nome da completo da minha mãe é' + nomeMae + ' ' +sobrenomeMae
+
+console.log(nomeCompletoMae)
+
+nomeCompletoMae = 'o nome da minha mãe é ${nomeMae} ${sobrenomeMae}'
+
+a
